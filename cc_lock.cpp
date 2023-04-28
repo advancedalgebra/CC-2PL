07026def7b8ace4da7a7079f3deb50151485ad91
caiwanli <651943559@qq.com>
2023-04-28 10:11:22 +0800

    update cc_lock.cpp comments

--- cc_lock.cpp
+++ cc_lock.cpp
@@ -5,31 +5,34 @@
 #include "global.h"
 #include "structure.h"
 
-/*
- * 1. 先判断数据项上的锁与自己加的锁是否冲突
- * 2. a. 若不冲突，对数据项上的mutex加锁，并设置数据上已有锁信息（owner）为自己加的锁
- *    b. 若冲突，将自己要加的锁加到该数据的等待队列waitlist中，并对数据mutex加锁，然后被阻塞，等待数据上已有的锁被释放。
- *       阻塞结束后，设置该数据上已有锁信息(owner)为自己加的锁，将自己从waitlist中移除。
- */
 RC cc_lock::lock_get(lock_t type, thread::id tid, Data &data){
+    RC rc = RCOK;
+    LockEntry lock1 = data.owner;
+    LockEntry lock2;
+    lock2.tid = tid;
+    lock2.type = type;
+    //1. 先判断数据项上的锁与自己加的锁是否冲突
+    //2. a. 若不冲突，对数据项上的mutex加锁，并设置数据上已有锁信息（owner）为自己加的锁
+    //   b. 若冲突，将自己要加的锁加到该数据的等待队列waitlist中，并对数据mutex加锁，然后被阻塞，等待数据上已有的锁被释放。
+    //      阻塞结束后，设置该数据上已有锁信息(owner)为自己加的锁，将自己从waitlist中移除。
+    return rc;
 
 }
 
-/*
- * 1. 判断数据项上有无阻塞的锁请求（检查waitlist）
- * 2. a. 若没有，先释放mutex，再将数据上的owner置为无锁
- *    b. 释放mutex上的锁
- */
 RC cc_lock::lock_release(lock_t type, thread::id tid, Data &data){
-
+    RC rc = RCOK;
+    //1. 判断数据项上有无阻塞的锁请求（检查waitlist）
+    //2. a. 若没有，先释放mutex，再将数据上的owner置为无锁
+    //   b. 释放mutex上的锁
+    return rc;
 }
 
-/*
- * 如果两个线程都是读锁，或者没有锁，则不冲突；否则都会发生冲突
- * lock1是数据上的锁，lock2是需要加的锁
- */
+//lock1是数据上的锁，lock2是需要加的锁
 bool cc_lock::conflict_lock(LockEntry lock1, LockEntry lock2) {
 
+    //1.首先判断是否属于同一事务的锁
+    //2.如果两个线程都是读锁，或者没有锁，则不冲突；否则都会发生冲突
+    return false;
 }
 
 RC cc_lock::insert(string key, int value, thread::id tid) {
